0ad585fcd52019de66c78105b95b456abf95bcb1
kethmi916 <kethmi.20232355@iit.ac.lk>
2026-04-22 10:22:08 +0530

    422 invalid data exception mapper

---
+++ src/main/java/com/api/mapper/InvalidDataMapper_422.java
@@ -0,0 +1,15 @@
+package com.api.mapper;
+
+import com.api.exception.InvalidDataException;
+
+import javax.ws.rs.core.*;
+import javax.ws.rs.ext.*;
+
+public class InvalidDataMapper_422 implements ExceptionMapper<InvalidDataException> {
+    @Override
+    public Response toResponse(InvalidDataException ex) {
+        return Response.status(422)
+                .entity(ex.getMessage())
+                .build();
+    }
+}
